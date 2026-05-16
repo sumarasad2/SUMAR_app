30b6adedc6f5e067f476511f42449fd2c6eb9cc9
sumarasad2 <sumarasad2@gmail.com>
2026-05-16 22:10:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -221,3 +221,15 @@ function clearForm() {
 }
 
 renderAppointments();
+
+if ("serviceWorker" in navigator) {
+
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(() => {
+
+      console.log(
+        "Service Worker Registered"
+      );
+    });
+}
